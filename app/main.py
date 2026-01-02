c93f145f8350eb79a8fbfb9ca1f722ebb39e1ed2
darkspirt1 <sahildighe8928@gmail.com>
2026-01-02 14:59:15 +0530

    after separation

--- app/main.py
+++ app/main.py
@@ -11,6 +11,8 @@ import time
 from .import models, schemas, utilis
 from .database import engine, get_db
 from sqlalchemy.orm import Session
+from .routers import posts, users
+
 models.Base.metadata.create_all(bind=engine)
 
 while True:
@@ -54,96 +56,8 @@ def root():
     return {"message": "Hello World"}
 
 
-# here we retrieve all posts from database
+app.include_router(posts.router)
+app.include_router(users.router)
 
 
-@app.get("/posts", status_code=status.HTTP_200_OK, response_model=list[schemas.Post])
-def get_posts(db: Session = Depends(get_db)):
-    # cursor.execute(""" SELECT * FROM posts""")
-    # posts = cursor.fetchall()
-    posts = db.query(models.Post).all()
-    return posts
 
-
-# here we define a route to create a post and add into database
-
-
-@app.post("/createpost", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
-def create_post(post: schemas.CreatePost, db: Session = Depends(get_db)):
-    # cursor.execute(""" INSERT INTO posts (title,content,published) VALUES (%s,%s,%s) RETURNING * """,
-    #                (post.title, post.content, post.published))
-    # new_post = cursor.fetchone()
-    # conn.commit()
-    new_post = models.Post(**post.dict())  # unpacking the post object
-    db.add(new_post)  # adding new post to session
-    db.commit()
-    db.refresh(new_post)  # to get the new post from database
-    return new_post
-
-
-# here we define a route to get a specific post by id from database
-
-@app.get("/posts/{id}", response_model=schemas.Post)
-def get_post(id: int, db: Session = Depends(get_db)):
-    post = db.query(models.Post).filter(models.Post.id == id).first()
-    if not post:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"post with id: {id} was not found")
-   # if not post:
-        # response.status_code = status.HTTP_404_NOT_FOUND
-        # return {"message": f"post with id: {id} was not found"}
-
-    print(post)
-    return post
-
-
-# here we will delete a post from database
-@app.delete("/posts/{id}",  status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int, db: Session = Depends(get_db)):
-    deleted_post = db.query(models.Post).filter(models.Post.id == id)
-    if not deleted_post.first():
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"post with id: {id} does not exist")
-    deleted_post.delete(synchronize_session=False)
-    db.commit()
-
-
-# here we will update a post in database
-
-
-@app.put("/posts/{id} , response_model=schemas.Post)")
-def update_post(id: int, updated_post: schemas.CreatePost, db: Session = Depends(get_db)):
-    post_query = db.query(models.Post).filter(models.Post.id == id)
-    post = post_query.first()
-    if post == None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"post with id: {id} does not exist")
-
-    post_query.update(updated_post.dict(), synchronize_session=False)
-    db.commit()
-    return updated_post
-
-# here we define a route to get a specific to create user
-
-
-@app.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-
-    # we hash the password - user.password
-    hashed_password = utilis.hash(user.password)
-    user.password = hashed_password
-    new_user = models.User(**user.dict())
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-    return new_user
-
-
-# here we define a route to get a specific user by id from database
-@app.get("/users/{id}", response_model=schemas.UserOut)
-def get_user(id: int, db: Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.id == id).first()
-    if  user == None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"user with id: {id} was not found")
-    return user
