7c79ca26b35033d841c1abc2cc0803bbeabbb589
darkspirt1 <sahildighe8928@gmail.com>
2026-01-03 17:43:23 +0530

    added owner relationship

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ from .database import Base
 from sqlalchemy import Column, Integer, String, Boolean , ForeignKey
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 from sqlalchemy.sql.expression import text
+from sqlalchemy.orm import relationship
 
 
 class Post(Base):
@@ -15,6 +16,8 @@ class Post(Base):
     created_at = Column(TIMESTAMP(timezone=True), nullable=False,
                        server_default=text('now()'))
     owner_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+
+    owner = relationship("User")
 
 
 
