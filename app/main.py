2730035175116b498ede14723306cf9cd58530b1
darkspirt1 <sahildighe8928@gmail.com>
2026-01-03 20:25:14 +0530

    clean up

--- app/main.py
+++ app/main.py
@@ -1,55 +1,12 @@
-from fastapi import FastAPI, Response, status, HTTPException, Depends
-from fastapi.params import Body
-from pydantic import BaseModel
-from typing import Optional, List
-from random import randrange
-import psycopg2
-from psycopg2.extras import RealDictCursor
-import time
-
-# importing models and schemas
-from .import models, schemas, utilis
-from .database import engine, get_db
-from sqlalchemy.orm import Session
+from fastapi import FastAPI
+from .import models
+from .database import engine
 from .routers import auth, posts, users
 
 models.Base.metadata.create_all(bind=engine)
 
-while True:
-    try:
-        conn = psycopg2.connect(host='localhost', database='postgres',
-                                user='postgres', password='8928', cursor_factory=RealDictCursor)
-        cursor = conn.cursor()
-        print("Database connection was successful")
-        break
-    except Exception as error:
-        print("Connecting to database failed")
-        print("Error: ", error)
-        time.sleep(5)
 
 app = FastAPI()
-
-
-# creating a list to hold our posts
-my_post = [{"title": "title of post 1", "content": "content of post 1", "id": 1},
-           {"title": "title of post 2", "content": "content of post 2", "id": 2}]
-
-# helper functions
-
-
-def find_post(id):
-    for p in my_post:
-        if p['id'] == id:
-            return p
-
-
-def find_index_post(id):
-    for i, p in enumerate(my_post):
-        if p['id'] == id:
-            return i
-
-# here we define our first route
-
 
 @app.get("/")
 def root():
