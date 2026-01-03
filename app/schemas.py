bc5e4f3b8a11735db02c6bac78778ee1fe40af38
darkspirt1 <sahildighe8928@gmail.com>
2026-01-03 13:26:38 +0530

    set up schema for tokens

--- app/schemas.py
+++ app/schemas.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel, EmailStr
-
+from typing import Optional
 
 class PostBase(BaseModel):
     title: str
@@ -29,3 +29,16 @@ class UserOut(BaseModel):
 
     class Config:
         from_attributes = True
+
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    id: Optional[str] = None
