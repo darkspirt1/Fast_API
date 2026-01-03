48634e91ce0f87b9d2078fa44e311b376a5db85d
darkspirt1 <sahildighe8928@gmail.com>
2026-01-03 16:59:01 +0530

    created foreign key

--- app/models.py
+++ app/models.py
@@ -1,5 +1,9 @@
+
 from .database import Base
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean , ForeignKey
+from sqlalchemy.sql.sqltypes import TIMESTAMP
+from sqlalchemy.sql.expression import text
+
 
 class Post(Base):
     __tablename__ = "posts"
@@ -8,3 +12,15 @@ class Post(Base):
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
     published = Column(Boolean, server_default='TRUE', nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False,
+                       server_default=text('now()'))
+    owner_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    password = Column(String, nullable=False)
